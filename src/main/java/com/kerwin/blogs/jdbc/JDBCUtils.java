31eb5d91f330ec1be787c51e76eaeb704fb42d18
kerwinJava <1464317397@qq.com>
2020-08-10 23:11:11 +0800

    学习笔记 1.mysql 关联查询优化器、排序优化 2.mysql驱动修改

--- src/main/java/com/kerwin/blogs/jdbc/JDBCUtils.java
+++ src/main/java/com/kerwin/blogs/jdbc/JDBCUtils.java
@@ -13,7 +13,7 @@ public class JDBCUtils {
 
     static{
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
 
         }
--- src/main/java/com/kerwin/blogs/jdbc/JDBCUtilsTest.java
+++ src/main/java/com/kerwin/blogs/jdbc/JDBCUtilsTest.java
@@ -1,12 +1,16 @@
 package com.kerwin.blogs.jdbc;
 
 import com.kerwin.blogs.BlogsApplicationTest;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.sql.Connection;
 
 public class JDBCUtilsTest extends BlogsApplicationTest {
 
     @Test
     public void getConnection() {
-        JDBCUtils.getInstance().getConnection();
+       Connection connection =  JDBCUtils.getInstance().getConnection();
+        Assert.assertNotNull(connection);
     }
 }
