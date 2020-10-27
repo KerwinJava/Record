ec548bdf9c258e7df764e7dd0fa5f7a7160a840b
KerwinJava <1464317397@qq.com>
2020-10-27 18:48:14 +0800

    Create ReverseInteger.java
    整数反转

---
+++ src/main/java/com/kerwin/leetcode/ReverseInteger.java
@@ -0,0 +1,21 @@
+package com.kerwin.leetcode;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        long res = 0;
+        do {
+            res = res * 10 + x % 10;
+        } while ((x = x / 10) != 0);
+        if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ReverseInteger().reverse(1534236469));
+        System.out.println(new ReverseInteger().reverse(1534236469));
+        System.out.println(Integer.MAX_VALUE == Math.pow(2, 31) - 1);
+        System.out.println(Integer.MAX_VALUE > 9646324351);
+    }
+}
